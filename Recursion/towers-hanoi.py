b791a1e27411ac46f5b0c66971bc5fd3c2726ae0
iTecX <30746447+iTecAI@users.noreply.github.com>
2020-02-07 11:37:20 -0500

    add more logging

--- Recursion/towers-hanoi.py
+++ Recursion/towers-hanoi.py
@@ -1,17 +1,30 @@
-def toh(n, r1, r2, r3, array):
+DCHART = {}
+
+def dchart(n,r1,r2,depth):
+    global DCHART
+    print(n,":",r1,"->",r2,depth)
+    if depth in DCHART.keys():
+        DCHART[depth].append(str(n)+r1+r2)
+    else:
+        DCHART[depth] = []
+        DCHART[depth].append(str(n)+r1+r2)
+
+
+def toh(n, r1, r2, r3, array, depth):
     if n == 1:
-        print(n,":",r1,"->",r2)
         array[r1].remove(n)
         array[r2].append(n)
+        dchart(n,r1,r2,depth)
         return array
-    toh(n-1, r1, r3, r2, array) 
-    print(n,":",r1,"->",r2)
+    toh(n-1, r1, r3, r2, array, depth+1) 
+    dchart(n,r1,r2,depth)
     array[r1].remove(n)
     array[r2].append(n)
-    toh(n-1, r3, r2, r1, array)
+    toh(n-1, r3, r2, r1, array, depth+1)
     return array
 
 def toh_factory(n):
+    global DCHART
     towers = dict(
         A=[],
         B=[],
@@ -19,10 +32,16 @@ def toh_factory(n):
     )
     for i in range(n):
         towers['A'].append(n-i)
-    return toh(n,'A','C','B',towers)
+    return toh(n,'A','C','B',towers,0)
 
-    
-ret = toh_factory(5)
+
+COUNT = 7
+
+ret = toh_factory(COUNT)
 print('\nResults\n=========================================')
+print('Recursions: ',sum([2 ** i for i in range(COUNT)]))
 for k in ret.keys():
     print(k,':',', '.join([str(i) for i in ret[k]]))
+print('')
+for k in range(len(DCHART.keys())):
+    print(k,':',', '.join([str(i) for i in DCHART[k]]))
